ddafd8a9fe599f8f1eb934ddf67ea23669b0495b
Bodhish Thomas <bodhish@gmail.com>
2019-12-29 23:56:41 +0530

    Remove images from testimonial section

--- packages/landing-gatsby/src/containers/Crypto/BannerSlider/index.js
+++ packages/landing-gatsby/src/containers/Crypto/BannerSlider/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
-import Image from 'gatsby-image';
-import Box from 'reusecore/src/elements/Box';
-import Text from 'reusecore/src/elements/Text';
-import Heading from 'reusecore/src/elements/Heading';
-import Container from 'common/src/components/UI/Container';
-import GlideCarousel from 'common/src/components/GlideCarousel';
-import GlideSlide from 'common/src/components/GlideCarousel/glideSlide';
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import Image from "gatsby-image";
+import Box from "reusecore/src/elements/Box";
+import Text from "reusecore/src/elements/Text";
+import Heading from "reusecore/src/elements/Heading";
+import Container from "common/src/components/UI/Container";
+import GlideCarousel from "common/src/components/GlideCarousel";
+import GlideSlide from "common/src/components/GlideCarousel/glideSlide";
 import TestimonialSecWrapper, {
   ImageWrapper,
-  TestimonialItem,
-} from './bannerSlider.style';
+  TestimonialItem
+} from "./bannerSlider.style";
 
 const TestimonialSection = ({
   reviewStyle,
   TestimonialMeta,
   nameStyle,
-  designationStyle,
+  designationStyle
 }) => {
   //Carousel Options
   const carouselOptions = {
-    type: 'carousel',
+    type: "carousel",
     autoplay: 3000,
     perView: 1,
-    animationDuration: 600,
+    animationDuration: 600
   };
 
   const Data = useStaticQuery(graphql`
@@ -49,7 +49,7 @@ const TestimonialSection = ({
   return (
     <TestimonialSecWrapper id="testimonial_section">
       <Container>
-        <div style={{ width: '440px' }}>
+        <div style={{ width: "440px" }}>
           <GlideCarousel
             options={carouselOptions}
             bullets={true}
@@ -62,7 +62,7 @@ const TestimonialSection = ({
                   <TestimonialItem className="testimonial_item">
                     <Text content={slideItem.review} {...reviewStyle} />
                     <Box {...TestimonialMeta}>
-                      <ImageWrapper>
+                      {/* <ImageWrapper>
                         <Image
                           fluid={
                             (slideItem.avatar !== null) | undefined
@@ -71,7 +71,7 @@ const TestimonialSection = ({
                           }
                           alt={`reviewer-image-${index}`}
                         />
-                      </ImageWrapper>
+                      </ImageWrapper> */}
                       <Box>
                         <Heading content={slideItem.name} {...nameStyle} />
                         <Text
@@ -95,36 +95,36 @@ TestimonialSection.propTypes = {
   reviewStyle: PropTypes.object,
   TestimonialMeta: PropTypes.object,
   nameStyle: PropTypes.object,
-  designationStyle: PropTypes.object,
+  designationStyle: PropTypes.object
 };
 
 TestimonialSection.defaultProps = {
   reviewStyle: {
-    fontSize: ['16px', '16px', '16px', '16px', '16px'],
-    fontWeight: '400',
-    color: 'rgb(82, 95, 127)',
-    lineHeight: '1.74',
-    mb: ['30px', '30px', '30px', '30px', '40px'],
+    fontSize: ["16px", "16px", "16px", "16px", "16px"],
+    fontWeight: "400",
+    color: "rgb(82, 95, 127)",
+    lineHeight: "1.74",
+    mb: ["30px", "30px", "30px", "30px", "40px"]
   },
   TestimonialMeta: {
     flexBox: true,
-    alignItems: 'center',
+    alignItems: "center"
   },
   nameStyle: {
-    as: 'h3',
-    fontSize: ['16px', '17px', '18px', '18px', '20px'],
-    fontWeight: '500',
-    color: 'rgb(50, 50, 93)',
-    mb: '5px',
-    fontFamily: 'Poppins',
+    as: "h3",
+    fontSize: ["16px", "17px", "18px", "18px", "20px"],
+    fontWeight: "500",
+    color: "rgb(50, 50, 93)",
+    mb: "5px",
+    fontFamily: "Poppins"
   },
   designationStyle: {
-    fontSize: '16px',
-    fontWeight: '400',
-    color: 'rgb(82, 95, 127)',
-    mb: '0',
-    fontFamily: 'Poppins',
-  },
+    fontSize: "16px",
+    fontWeight: "400",
+    color: "rgb(82, 95, 127)",
+    mb: "0",
+    fontFamily: "Poppins"
+  }
 };
 
 export default TestimonialSection;
